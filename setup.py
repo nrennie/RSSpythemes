e95bea30db940c2ddd3d9952ab920588f4cc97c4
nrennie <nrennie35@gmail.com>
2023-08-26 19:11:12 +0100

    add setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+DESCRIPTION = 'RSS Palettes and Themes'
+LONG_DESCRIPTION = 'Defines colour palettes and themes for RSS publications.'
+
+# Setting up
 setup(
-    name="RSSpythemes",
-    version="1.0",
-    packages=["RSSpythemes"],
-    install_requires=[
-        "colour>=0.1.5",
-        "matplotlib>=3.5.1"
-    ]
+        name="RSSpythemes", 
+        version="1.0.0",
+        author="Nicola Rennie",
+        author_email="nrennie35@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent"
+        ]
 )
